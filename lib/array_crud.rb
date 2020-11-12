1f0b2a80b50e491f2db9268c99f3127ace6e1e1a
matthewjstahnke <matthewjstahnke@gmail.com>
2020-11-12 02:48:18 -0600

    commit tree 0cee83ac9b427a1692859c75988becffb07470c1

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
 def create_an_empty_array
-  
-end
 
-def create_an_array
+    []  
+  end
   
-end
-
-def add_element_to_end_of_array(array, element)
+  def create_an_array
   
-end
-
-def add_element_to_start_of_array(array, element)
+   ["One", 2, true, :four] 
+  end
   
-end
-
-def remove_element_from_end_of_array(array)
+  def add_element_to_end_of_array(array, element)
   
-end
-
-def remove_element_from_start_of_array(array)
+    array << element  
+  end
   
-end
-
-def retrieve_element_from_index(array, index_number)
+  def add_element_to_start_of_array(array, element)
   
-end
-
-def retrieve_first_element_from_array(array)
+     array.unshift(element) 
+  end
   
-end
-
-def retrieve_last_element_from_array(array)
+  def remove_element_from_end_of_array(array)
   
-end
+    array.pop  
+  end
+  
+  def remove_element_from_start_of_array(array)
+  
+    array.shift 
+  end
+  
+  def retrieve_element_from_index(array, index_number)
+  
+    array[index_number] 
+  end
+  
+  def retrieve_first_element_from_array(array)
+  
+    array.first 
+  end
+  
+  def retrieve_last_element_from_array(array)
+  
+    array.last  
+  end
